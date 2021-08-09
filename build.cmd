a6b49d501e89b20c0192bdd7824ccf49566173f5
Eirik Th S <eirik@flowtivity.io>
2021-08-09 17:57:29 +0200

    Added windows-scrips and updated README

--- build.cmd
+++ build.cmd
@@ -1,20 +1,19 @@
-#!/bin/bash
+@echo off
 
-# Kill running instance of tomcat
+:: Kill running instance of tomcat
 docker kill tomcat
 
-# Compile and packages source code into .war file via maven volume in docker container
+:: Compiles and packages source code into .war file via maven volume in docker container
 docker run --rm -it --name mavenbuild -v maven-repo:/root/.m2 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven mvn clean install
 
-# Copy and rebuilds tomcat image with latest .war file
+:: Copies and rebuilds tomcat image with latest .war file
 docker image build -t trym/tomcat .
 
-# Start tomcat container, making the webapp available.
+:: Starts tomcat container, making the webapp available.
 docker container run --rm -it -d --name tomcat --publish 8081:8080 trym/tomcat
 
-echo ""
+echo.
 echo "Link: http://localhost:8081/bacit-web-1.0-SNAPSHOT/"
-echo ""
+echo.
 
-read -n 1 -s -r -p "Press any key to continue..."
-echo ""
+pause
