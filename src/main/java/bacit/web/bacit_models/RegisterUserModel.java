cebd5e520d0ca586240c33117beed306a51f8661
HennningS <henningsletner.70@gmail.com>
2021-10-10 14:06:38 +0200

    La til en servlets
    La til registeruserservlet og modeler

---
+++ src/main/java/bacit/web/bacit_models/RegisterUserModel.java
@@ -0,0 +1,22 @@
+package bacit.web.bacit_models;
+
+public class RegisterUserModel {
+    private String fullName;
+    private String phoneNumber;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/bacit/web/bacit_web/RegisterUserServlet.java
@@ -0,0 +1,102 @@
+package bacit.web.bacit_web;
+import bacit.web.bacit_models.RegisterUserModel;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet(name = "registerUserServlet", value = "/register_user")
+public class RegisterUserServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        writeCreateUserForm(out,null);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        RegisterUserModel user = new RegisterUserModel();
+        user.setFullName(request.getParameter("fullName"));
+        user.setPhoneNumber(request.getParameter("phoneNumber"));
+        PrintWriter out = response.getWriter();
+        if(validateUser(user)){
+            try{
+                writeUserToDb(user, out);
+            }
+            catch (SQLException | ClassNotFoundException ex){
+                out.println(ex.getMessage());
+            }
+            writeHtmlStart(out, "Register user response");
+            out.println("User with fullname: "+user.getFullName()+" and phone number: "
+                    +user.getPhoneNumber()+" registered");
+            writeHtmlEnd(out);
+        }
+        else
+        {
+            writeCreateUserForm(out, "Validation failed");
+        }
+    }
+
+    private void writeUserToDb(RegisterUserModel user,PrintWriter out) throws SQLException, ClassNotFoundException {
+        Connection db = DBUtils.getINSTANCE().getConnection(out);
+        String insertUserCommand = "insert into users (User_fullName, User_phoneNumber) values(?,?);";
+        PreparedStatement statement = db.prepareStatement(insertUserCommand);
+        statement.setString(1, user.getFullName());
+        statement.setString(2, user.getPhoneNumber());
+
+        statement.executeUpdate();
+    }
+
+    private void writeCreateUserForm(PrintWriter out, String errorMessage) {
+        writeHtmlStart(out, "Registrer bruker");
+        if(errorMessage!=null)
+        {
+            out.println("<h3>"+errorMessage+"</h3>");
+        }
+        out.println("<form action='register_user' method='POST'>");
+        out.println("<label for='fullName'>Fullt navn</label> ");
+        out.println("<input type='text' name='fullName'/>");
+        out.println("<label for='phoneNumber'>Telefonnummer</label> ");
+        out.println("<input type='tel' name='phoneNumber'/>");
+        out.println("<input type='submit' value='Registrer bruker'/>");
+        out.println("</form>");
+        writeHtmlEnd(out);
+    }
+
+    private void writeHtmlStart(PrintWriter out, String title) {
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>"+title+"</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h2>"+title+"</h2>");
+    }
+    private void writeHtmlEnd(PrintWriter out) {
+        out.println("</body>");
+        out.println("</html>");
+    }
+    private Boolean validateUser(RegisterUserModel model){
+        if(model.getFullName()==null)
+            return false;
+        if(model.getFullName().trim().equalsIgnoreCase(""))
+            return false;
+        if(model.getPhoneNumber()==null)
+            return false;
+        if(model.getPhoneNumber().trim().equalsIgnoreCase(""))
+            return false;
+        return true;
+    }
+}
