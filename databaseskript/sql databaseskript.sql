b7c8a47162803cf7ab83a8355b05a5d927fb4f96
Henroed <79584268+Henroed@users.noreply.github.com>
2021-10-29 09:30:00 +0200

    dataskript

--- databaseskript/sql databaseskript.sql
+++ databaseskript/sql databaseskript.sql
@@ -4,12 +4,27 @@ Use Gruppe9;
 
 CREATE TABLE Brukere (Bruker_ID integer UNIQUE auto_increment,
                       Fult_navn Varchar(255),
-                      Telefonnummer Varchar(8),
+                      Telefonnummer Varchar(15),
                       E_post Varchar(30),
                       Fagforbund BOOLEAN,
                       Brukernavn VARCHAR(30),
                       Passord Varchar(50)
+
 );
+
+Alter table Brukere Add column Bruker_roller Varchar(25);
+
+CREATE INDEX Fult_navn
+    ON Brukere (Fult_navn);
+
+ALTER TABLE Brukere
+    add CONSTRAINT telefon
+    check (Telefonnummer >= 8);
+
+CREATE VIEW bruker_admin AS
+    SELECT Fult_navn, Telefonnummer, E_post
+    From Brukere
+    where Bruker_roller = 'admin';
 
 Alter table Brukere add primary key (Bruker_ID);
 
@@ -39,12 +54,18 @@ CREATE TABLE Produkter (
                            Produkt_ID integer UNIQUE auto_increment,
                            Produkt_navn VARCHAR(50),
                            Kategori VARCHAR(50),
+                           Beskrivelse VARCHAR(500),
+                           Bilde blob,
+                           Brukes boolean,
                            Kurs_ID INTEGER,
                            PRIS_ID INTEGER,
                            PRIMARY KEY (Produkt_ID),
                            FOREIGN KEY (Kurs_ID) REFERENCES Kurs(Kurs_ID),
                            FOREIGN KEY (PRIS_ID) REFERENCES Pris(Pris_ID)
 );
+
+CREATE INDEX Produkt_index
+    ON Produkter (Produkt_navn, Kategori);
 
 CREATE TABLE Reservasjoner (
                                Reservasjon_ID integer UNIQUE auto_increment,
@@ -60,4 +81,132 @@ CREATE TABLE Reservasjoner (
 ALTER TABLE Brukere MODIFY COLUMN Telefonnummer VARCHAR(11);
 
 ALTER TABLE Brukere DROP COLUMN Brukernavn;
+
+SELECT (Produkt_navn, Beskrivelse, Bilde) FROM Produkter AS P_info;
+
+SELECT * FROM Brukere, Produkter, Kurs, Pris, Reservasjoner
+LIMIT 5;
+
+Select Produkt_navn from Produkter where brukes = 1;
+
+ALTER Table reservasjoner Add column dato_now datetime default now();
+
+Select fult_navn from brukere inner join reservasjoner
+on brukere.bruker_id = reservasjoner.bruker_id
+inner join produkter on Reservasjoner.Produkt_ID = Produkter.Produkt_ID
+where reservasjoner.reserasjon_dato_til < reservasjoner.dato_now And brukes = 1;
+
+
+
+
+
+
+INSERT INTO Gruppe9.Brukere (Bruker_ID, Fult_navn, Telefonnummer, E_post, Fagforbund, Passord, Bruker_roller) VALUES (2, 'Nora Fure', '+4777366390', 'Nora@Epost.com', 1, '123', 'Bruker');
+
+INSERT INTO Gruppe9.Brukere (Bruker_ID, Fult_navn, Telefonnummer, E_post, Fagforbund, Passord, Bruker_roller) VALUES (3, 'Hernik Roed', '+4789771324', 'Henrik@Epost.com', 0, '1234', 'Admin');
+
+INSERT INTO Gruppe9.Brukere (Bruker_ID, Fult_navn, Telefonnummer, E_post, Fagforbund, Passord, Bruker_roller) VALUES (4, 'Mads Teland', '+4790001982', 'Mads@Epost.com', 0, '12345', 'Bruker');
+
+INSERT INTO Gruppe9.Brukere (Bruker_ID, Fult_navn, Telefonnummer, E_post, Fagforbund, Passord, Bruker_roller) VALUES (5, 'Emanuel Minak', '+4724524372', 'Emanuel@Epost.com', 1, '123456', 'Bruker');
+
+INSERT INTO Gruppe9.Brukere (Bruker_ID, Fult_navn, Telefonnummer, E_post, Fagforbund, Passord, Bruker_roller) VALUES (6, 'Knut Norwegian', '+4775312387', 'Knut@Epost.com', 1, '654321', 'Admin');
+
+INSERT INTO Gruppe9.Brukere (Bruker_ID, Fult_navn, Telefonnummer, E_post, Fagforbund, Passord, Bruker_roller) VALUES (7, 'Roald Peroson', '+4783928173', 'Roald@Epost.com', 0, '54321', 'Bruker');
+
+INSERT INTO Gruppe9.Brukere (Bruker_ID, Fult_navn, Telefonnummer, E_post, Fagforbund, Passord, Bruker_roller) VALUES (8, 'Frida Karlsen', '+4712763521', 'Frida@Epost.com', 0, '4321', 'Bruker');
+
+INSERT INTO Gruppe9.Brukere (Bruker_ID, Fult_navn, Telefonnummer, E_post, Fagforbund, Passord, Bruker_roller) VALUES (9, 'Ingrid Benthus', '+4721379862', 'Ingrid@Epost.com', 1, '321', 'Bruker');
+
+INSERT INTO Gruppe9.Brukere (Bruker_ID, Fult_navn, Telefonnummer, E_post, Fagforbund, Passord, Bruker_roller) VALUES (10, 'Roar Klippestad', '+4721875362', 'Roar@Epost.com', 0, 'Passord', 'Admin');
+
+INSERT INTO Gruppe9.Pris (Pris_ID, Pris) VALUES (10, 0);
+
+INSERT INTO Gruppe9.Pris (Pris_ID, Pris) VALUES (20, 50);
+
+INSERT INTO Gruppe9.Pris (Pris_ID, Pris) VALUES (30, 100);
+
+INSERT INTO Gruppe9.Pris (Pris_ID, Pris) VALUES (40, 150);
+
+INSERT INTO Gruppe9.Admin (Admin_ID, Bruker_ID) VALUES (100, 3);
+
+INSERT INTO Gruppe9.Admin (Admin_ID, Bruker_ID) VALUES (200, 6);
+
+INSERT INTO Gruppe9.Admin (Admin_ID, Bruker_ID) VALUES (300, 10);
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (1000, 1, 'Førerkort Klasse B', '1997-01-17');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (1001, 2, 'Førerkort Klasse B', '1982-06-14');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (1002, 3, 'Førerkort Klasse B', '1998-11-10');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (1003, 4, 'Førerkort Klasse B', '2000-06-01');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (1004, 5, 'Førerkort Klasse B', '1994-02-21');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (1005, 6, 'Førerkort Klasse B', '1996-09-20');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (1006, 7, 'Førerkort Klasse B', '1985-04-16');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (1007, 8, 'Førerkort Klasse B', '1979-12-12');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (1008, 9, 'Førerkort Klasse B', '1991-09-13');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (2000, 2, 'Tilhenger B96', '2001-12-11');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (2001, 4, 'Tilhenger B96', '2015-08-15');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (2002, 5, 'Tilhenger B96', '2016-06-15');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (2003, 8, 'Tilhenger B96', '2008-10-10');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (3000, 1, 'KranFører G4', '2005-02-01');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (3001, 4, 'KranFører G4', '2012-08-15');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (3002, 7, 'KranFører G4', '2013-10-11');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (4000, 8, 'ranfører G20', '2008-09-11');
+
+INSERT INTO Gruppe9.Kurs (Kurs_ID, Bruker_ID, Kurs_navn, Kurs_dato_fullført) VALUES (4001, 4, 'ranfører G20', '2018-03-12');
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10001, 'Liten Drill', 'Små verktøy', null, 10, null);
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10002, 'Liten Drill', 'Små Verktøy', null, 10, 1);
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10003, 'Tilhenger(Liten)', 'Transport', null, 10, 0);
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10004, 'Tillhenger (Stor)', 'Transport', null, 10, 1);
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10005, 'Lyft', 'Maskiner', null, 10, null);
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10006, 'Dekkskiftemaskin', 'Maskiner', null, 10, 0);
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10007, 'Slagdrill', 'Små Verktøy', null, 10, 1);
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10008, 'Jekktralle', 'Transport', null, 10, 0);
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10009, 'Verktøykasse', 'Små Verktøy', null, 10, 1);
+
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Kurs_ID, PRIS_ID, Brukes) VALUES (10010, 'Sementmikser', 'Maskiner', null, 10, 0);
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (11, 10001, 1, '2021-10-26', '2021-10-29');
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (12, 10002, 2, '2021-10-05', '2021-10-07');
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (13, 10005, 7, '2021-11-09', '2021-11-11');
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (14, 10009, 1, '2021-10-26', '2021-10-29');
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (15, 10010, 9, '2021-12-27', '2021-12-29');
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (16, 10004, 2, '2021-10-01', '2021-10-03');
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (17, 10006, 1, '2021-10-29', '2021-10-31');
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (18, 10008, 5, '2021-10-27', '2021-10-30');
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (19, 10008, 7, '2022-01-14', '2022-01-15');
+
+INSERT INTO Gruppe9.Reservasjoner (Reservasjon_ID, Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES (21, 10004, 3, '2021-06-10', '2021-06-13');
+
 
--- src/main/java/bacit/web/bacit_web/ProductListServlet.java
+++ src/main/java/bacit/web/bacit_web/ProductListServlet.java
@@ -1,5 +1,6 @@
 package bacit.web.bacit_web;
 import bacit.web.bacit_models.ProductListModel;
+import bacit.web.bacit_models.RegisterUserModel;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,8 +22,69 @@ public class ProductListServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         writeHtmlStart(out, "Product liste");
+        //writeProductlist(out);
+
+        out.println("<section id=\"boxes\">\n" +
+                "    <div class=\"container\">\n" +
+                "        <article id=\"main-col\">\n" +
+                "            <h1 class=\"page-title\">Iskaffe</h1>\n" +
+                "            <ul id=\"Iskaffe\">\n" +
+                "                <li>\n" +
+                "    <h3>Hva er det?</h3>\n" +
+                "    <p>Enkelt forklart er iskaffe akkurat det samme som varm kaffe den eneste store forskjellen er at den er servert kald. Det er den perfekte drikken til en varm dag, dersom du ønsker å holde deg våken mens du soler deg. Dersom du spør etter en \"Iced coffee\" vil du få kaffe blandet med melk, istedenfor vann, noe som vil dempe kaffesmaken.</p>\n" +
+                "                </li>\n" +
+                "                <li>\n" +
+                "    <h3>Iced Americano</h3>\n" +
+                "    <p> Dette er en av mine personlige favoritter, ettersom at det er en tydelig kaffesmak, den er lett å drikke og det er relativt få kalorier i den. Dette er også den kaffetypen som medlemmene i BTS har blitt sett drikke mest av, noe som kan være grunnen til at den har blitt så populær.</p>\n" +
+                "                </li>\n" +
+                "                <li>\n" +
+                "    <h3>Cold Brew</h3>\n" +
+                "    <p>Dette er en annen av mine personlige favoritter. Det er litt set samme som en iced americano, bare at kaffen er brygget kaldt og over lengere tid. Denne prossesen gjør at kaffen ikke får den bittere smaken kaffe er kjent for å ha. Istedenfor vil kaffen være litt søtere, noe som vil gjøre den enda enklere å drikke.</p>\n" +
+                "                </li>\n" +
+                "            </ul>\n" +
+                "        </article>\n" +
+                "    </div>\n" +
+                "</section>");
+        writeCreateUserForm(out,null);
+
+        writeHtmlEnd(out);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        ProductListModel user = new ProductListModel();
+        user.setProductName(request.getParameter("productName"));
+        user.setProductDescription(request.getParameter("description"));
+        user.setProductPicture(request.getParameter("picture"));
+        PrintWriter out = response.getWriter();
+        if (validateUser(product)) {
+            try {
+                writeUserToDb(product, out);
+            } catch (SQLException | ClassNotFoundException ex) {
+                out.println(ex.getMessage());
+            }
+            {
+                writeCreateUserForm(out, "Validation failed");
+            }
+
+        }
+    }
+        private void writeUserToDb(RegisterUserModel user,PrintWriter out) throws SQLException, ClassNotFoundException {
+            Connection db = DBUtils.getINSTANCE().getConnection(out);
+            String insertUserCommand = "insert into Brukere (Fult_navn, Telefonnummer, E_post, Fagforbund, Passord) values(?,?,?,?,?);";
+            PreparedStatement statement = db.prepareStatement(insertUserCommand);
+            statement.setString(1, user.getFullName());
+            statement.setString(2, user.getPhoneNumber());
+            statement.setString(3, user.geteMail());
+            statement.setBoolean(4, user.getFagforbund());
+            statement.setString(5, user.getPassword());
+
+            statement.executeUpdate();
+        }
+    /* private void writeProductlist(PrintWriter out) {
         while (rs.next()) {
-        //Print one row
+            //Print one row
             for(int i = 1 ; i <= 29; i++) {
                 out.println("<section id=\"boxes\">\n" +
                         "    <div class=\"container\">\n" +
@@ -36,14 +98,27 @@ public class ProductListServlet extends HttpServlet {
                         "        </article>\n" +
                         "    </div>\n" +
                         "</section>");
-                writeHtmlEnd(out);
+
             }
         }
     }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
+    */
+    private void writeCreateUserForm(PrintWriter out, String errorMessage) {
+        if (errorMessage != null) {
+            out.println("<h3>" + errorMessage + "</h3>");
+        }
+        out.println("<form action='register_user' method='POST'>");
+        out.println("<label for='produktnavn'>Produkt navn</label>");
+        out.println("<input type='text' name='Produktnavn'/>");
+        out.println("<br>");
+        out.println("<label for='beskrivelse'>Beskrivelse</label>");
+        out.println("<input type='text' name='Beskrivelse'/>");
+        out.println("<br>");
+        out.println("<label for='bilde'>bilde</label>");
+        out.println("<input type='text' name='Bilde'/>");
+        out.println("<br>");
+        out.println("<input type='submit' value='Legg til produkt'/>");
+        out.println("</form>");
     }
     private void writeHtmlStart(PrintWriter out, String title) {
         out.println("<html><head>\n" +
@@ -73,5 +148,27 @@ public class ProductListServlet extends HttpServlet {
                 "</footer>\n" +
                 "</body>\n" +
                 "</html>");
+    }
+    private Boolean validateUser(RegisterUserModel model){
+        if(model.getFullName()==null)
+            return false;
+        if(model.getFullName().trim().equalsIgnoreCase(""))
+            return false;
+        if(model.getPhoneNumber()==null)
+            return false;
+        if(model.getPhoneNumber().trim().equalsIgnoreCase(""))
+            return false;
+        if(model.geteMail()==null)
+            return false;
+        if(model.geteMail().trim().equalsIgnoreCase(""))
+            return false;
+        if(model.getFagforbund()==null)
+            return false;
+        if(model.getPassword()==null)
+            return false;
+        if(model.getPassword().trim().equalsIgnoreCase(""))
+            return false;
+
+        return true;
     }
     }
