0cb8070d5e2eb67d5389cc4343cc11c6f70201ec
Henroed <79584268+Henroed@users.noreply.github.com>
2021-11-03 12:18:19 +0100

    Registrer produkt

---
+++ src/main/java/bacit/web/bacit_models/RegisterProductModel.java
@@ -0,0 +1,21 @@
+package bacit.web.bacit_models;
+
+public class RegisterProductModel {
+    private String productName;
+    private String category;
+    private String beskrivelse;
+
+    public String getproductName() { return productName; }
+
+    public void setProductName(String productName) { this.productName = productName; }
+
+    public String getCategory() { return category; }
+
+    public void setCategory(String category) { this.category = category; }
+
+    public String getBeskrivelse() { return beskrivelse; }
+
+    public void setBeskrivelse(String beskrivelse) { this.beskrivelse = beskrivelse; }
+}
+
+
