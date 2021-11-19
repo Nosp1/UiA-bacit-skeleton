d791ae30ca7ebaeeeb7492f01f4665328e0a2abb
mads1153 <79578621+mads1153@users.noreply.github.com>
2021-11-19 14:27:03 +0100

    Fjernet Kurs_ID  og alle null i linjene 150-159 i INSERT INTO Gruppe9.Produkter
    Fjernet Kurs_ID  og alle null i linjene 150-159 i INSERT INTO Gruppe9.Produkter.

--- databaseskript/sql databaseskript.sql
+++ databaseskript/sql databaseskript.sql
@@ -146,17 +146,17 @@ VALUES
 (3, 6, '2018-11-16'),
 (4, 6, '2013-02-22');
 
-INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Beskrivelse, Bilde Kurs_ID, PRIS_ID, Brukes) VALUES
-(10001, 'Liten Drill', 'Små Verktøy', 'Lang text', null, 2, 0,null),
-(10002, 'Liten Drill', 'Små Verktøy', 'Lang text', null, 2, 1, null),
-(10003, 'Tilhenger(Liten)', 'Transport', 'Lang text', null, 3, 0, null),
-(10004, 'Tillhenger (Stor)', 'Transport', 'Lang text', null, 4, 1, null),
-(10005, 'Lyft', 'Maskiner', 'Lang text', null, 4, 1, null),
-(10006, 'Dekkskiftemaskin', 'Maskiner', 'Lang text', null, 4, 0, null),
-(10007, 'Slagdrill', 'Små Verktøy', 'Lang text', null, 2, 1, null),
-(10008, 'Jekktralle', 'Transport', 'Lang text', null, 2, 0, null),
-(10009, 'Verktøykasse', 'Små Verktøy', 'Lang text', null, 2, 1, null),
-(10010, 'Sementmikser', 'Maskiner', 'Lang text', null, 2, 0, null);
+INSERT INTO Gruppe9.Produkter (Produkt_ID, Produkt_navn, Kategori, Beskrivelse, Bilde, PRIS_ID, Brukes) VALUES
+(10001, 'Liten Drill', 'Små Verktøy', 'Lang text', 2, 0),
+(10002, 'Liten Drill', 'Små Verktøy', 'Lang text', 2, 1),
+(10003, 'Tilhenger(Liten)', 'Transport', 'Lang text', 3, 0),
+(10004, 'Tillhenger (Stor)', 'Transport', 'Lang text' 4, 1),
+(10005, 'Lyft', 'Maskiner', 'Lang text', 4, 1),
+(10006, 'Dekkskiftemaskin', 'Maskiner', 'Lang text', 4, 0),
+(10007, 'Slagdrill', 'Små Verktøy', 'Lang text', 2, 1),
+(10008, 'Jekktralle', 'Transport', 'Lang text', 2, 0),
+(10009, 'Verktøykasse', 'Små Verktøy', 'Lang text', 2, 1),
+(10010, 'Sementmikser', 'Maskiner', 'Lang text', 2, 0);
 
 INSERT INTO Gruppe9.Reservasjoner (Produkt_ID, Bruker_ID, Reservasjon_dato_fra, Reserasjon_dato_til) VALUES
 (10001, 1, '2021-10-26', '2021-10-29'),
